69dcdddb20e08914b156af857a9ce1af6ae6b088
arturo22corral <77382673+arturo22corral@users.noreply.github.com>
2021-09-22 12:08:00 +0200

    º
    ºº

---
+++ src/Concatenacion/Concatenacion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Concatenacion;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Concatenacion {
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+    }
+    public class Concatenacion{
+        
+           public static void main (String args[]){
+               var alumno = "Alba";
+               var  titulo = "Informático";
+               
+               var contact = titulo + " " + alumno;
+               System.out.println("contact = " + conctact);
+           }
+    }
